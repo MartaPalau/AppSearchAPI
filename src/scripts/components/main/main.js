a8053dfb2f9f8f7ed25c58b4592a471cd5ce704d
RED\mpalau <mpalau@alfatecsistemas.es>
2018-06-04 00:18:19 +0200

    clean project

--- src/scripts/components/main/main.js
+++ src/scripts/components/main/main.js
@@ -1,11 +1,7 @@
 (function(app) {
     'use strict';
-    mainCtrl.$inject = ['$scope', '$http'];
-    function mainCtrl(){
-    }
     var mainComponent = {
         templateUrl: "scripts/components/main/main.html",
-        controller: mainCtrl
     };
     app.component("main", mainComponent);
 })(angular.module("testFront"));
