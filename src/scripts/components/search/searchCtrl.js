80c184b8bc89d7729b28b3fb3bd25ed3ffb04869
RED\mpalau <mpalau@alfatecsistemas.es>
2018-06-06 00:19:06 +0200

    data persistence and details

--- src/scripts/components/search/searchCtrl.js
+++ src/scripts/components/search/searchCtrl.js
@@ -1,49 +1,74 @@
 (function (app) {
     'use strict';
 
-    searchCtrl.$inject = ['$http','$scope','angularGridInstance','$localStorage','searchService','toastr'];
-    function searchCtrl($http,$scope,angularGridInstance,$localStorage,searchService,toastr){
-        var vm = this;
+    searchCtrl.$inject = ['$scope','angularGridInstance','$localStorage','searchService','toastr'];
+    function searchCtrl($scope,angularGridInstance,$localStorage,searchService,toastr){
+        //init variables from controller and image routes
+        var vm = this,
+        imgBaseUrl = 'http://images-assets.nasa.gov/image/',
+        videoBaseUrl = 'http://images-assets.nasa.gov/video/',
+        imgThumbExt = '~thumb.jpg',
+        imgLargeExt = '~small.jpg',
+        videoThumbExt = '~small_thumb_00002.png',
+        videoMediaExt = '~small.mp4';
 
         vm.$onInit = function(){
+            //init parameters object as empty array
             vm.assetData = [];
             vm.results = false;
             vm.showLoading = false;
             vm.mediaType = [
                 {
-                    type: 'image',
-                    checked: true,
-                    label: 'Images'
+                    id: 1,
+                    type: 'image'
                 },
                 {
-                    type: 'video',
-                    checked: true,
-                    label: 'Videos'
+                    id: 2,
+                    type: 'video'
                 }
             ];
-
-            $scope.refresh = function(){
-                angularGridInstance.gallery.refresh();
-            };
-
+            //init both media type as true
+            vm.initMediaType();
+            //init storage data
             vm.getQueryStorage();
         };
-
-        vm.getQueryStorage = function(){
-            if($localStorage.searchQuery){
-                vm.getQuery($localStorage.searchQuery);
-                vm.search = $localStorage.searchQuery;
-            }
+        //refresh image masonry gallery
+        $scope.refresh = function(){
+            angularGridInstance.gallery.refresh();
         };
 
-        vm.mediaTypeQuery = function(index){
-            angular.forEach(vm.mediaType, function (type, key) {
-                if(index == key) {
-                    (type.checked) ? type.checked = false : type.checked = true;
-                }
+        vm.initMediaType = function(){
+            angular.forEach(vm.mediaType, function (typeId) {
+                typeId.id = true;
             });
         };
 
+        vm.getQueryStorage = function(){
+            if($localStorage.mediaTypeIdSelected){
+                angular.forEach($localStorage.mediaTypeIdSelected, function (typeId,key) {
+                    vm.mediaType[key].id = typeId;
+                });
+            }
+            if($localStorage.mediaTypeSelected){
+                vm.mediaSelected = $localStorage.mediaTypeSelected;
+            }
+            if($localStorage.searchQuery){
+                vm.getQuery($localStorage.searchQuery);
+                vm.search = $localStorage.searchQuery;
+                vm.showLoading = true;
+            }
+        };
+        //save media type selected
+        vm.mediaTypeQuery = function(index){
+            $localStorage.mediaTypeIdSelected = [];
+            angular.forEach(vm.mediaType, function (type, key) {
+                if(index == key) {
+                    type.id = !type.id;
+                }
+                $localStorage.mediaTypeIdSelected.push(type.id);
+            });
+        };
+        //get search query http get comes from searchService service file
        vm.getQuery = function (search){
             vm.setMediaType();
             vm.showLoading = true;
@@ -56,12 +81,13 @@
                    vm.noResults(vm.collItem);
                    vm.iterateAssetItems(vm.collItem);
                },function(){
-                   toastr.error('ERROR');
-               });
+               toastr.error('ERROR');
+           });
+           }else{
+               vm.searchIsEmpty(search);
            }
-           vm.searchIsEmpty(search);
         };
-
+        //iterate assets to generate asset object with useful data
         vm.iterateAssetItems = function(collItem){
             angular.forEach(collItem, function (item) {
                 var data = item.data;
@@ -69,52 +95,60 @@
                 vm.assetData.push({assetId: vm.id, assetHref: vm.assetHref, description: vm.description, title: vm.title, media: vm.media});
             });
         };
-
+        //bolean parameter to control search results
         vm.noResults = function(assetItems){
-            (assetItems.length > 0) ? vm.results = false : vm.results = true;
+            vm.results = (assetItems.length > 0) ? false : true;
         };
-
+        //bolean parameter to control input
         vm.searchIsEmpty = function(search){
-            (search) ? vm.isEmpty = false : vm.isEmpty = true;
+            vm.isEmpty = (search)? false : true;
             vm.showLoading = false;
         };
-
+        //set media type for search query
         vm.setMediaType = function(){
             vm.mediaSelected = [];
             angular.forEach(vm.mediaType, function (type) {
-                vm.mediaTypeActive = type.checked;
-                if(vm.mediaTypeActive){
+                if(type.id){
                     vm.mediaSelected.push(type.type).toString();
+                    $localStorage.mediaTypeSelected = vm.mediaSelected;
                 }
             });
         };
-
+        //get useful asset information
         vm.getAssetData = function(data){
             angular.forEach(data, function (itemData) {
                 vm.id = itemData.nasa_id;
-                vm.description =  itemData.description;
-                vm.title =  itemData.title;
+                vm.description = itemData.description;
+                vm.title = itemData.title;
                 vm.media = itemData.media_type;
                 vm.getAssetSrc(vm.media, vm.id);
             });
         };
-
+        //get assets source based on type
         vm.getAssetSrc = function(mediaType, id){
             if(mediaType == 'image'){
-                vm.assetThumb = 'http://images-assets.nasa.gov/image/' + id + '/' + id + '~thumb.jpg';
-                vm.assetLarge = 'http://images-assets.nasa.gov/image/' + id + '/' + id + '~small.jpg';
-                vm.assetHref = {
-                    assetThumb: vm.assetThumb,
-                    assetLarge: vm.assetLarge
-                };
+                vm.imageSrc(id);
             } else if (mediaType == 'video'){
-                vm.assetThumbPng = 'http://images-assets.nasa.gov/video/' + id + '/' + id + '~small_thumb_00002.png';
-                vm.assetVideo = 'http://images-assets.nasa.gov/video/' + id + '/' + id + '~small.mp4';
-                vm.assetHref = {
-                    assetThumb: vm.assetThumbPng,
-                    assetVideo: vm.assetVideo
-                };
+                vm.videoSrc(id);
             }
+        };
+        //set image sources
+        vm.imageSrc = function(id){
+            vm.assetThumb = imgBaseUrl + id + '/' + id + imgThumbExt;
+            vm.assetLarge = imgBaseUrl + id + '/' + id + imgLargeExt;
+            vm.assetHref = {
+                assetThumb: vm.assetThumb,
+                assetLarge: vm.assetLarge
+            };
+        };
+        //set video sources
+        vm.videoSrc = function(id){
+            vm.assetThumbPng = videoBaseUrl + id + '/' + id + videoThumbExt;
+            vm.assetVideo = videoBaseUrl + id + '/' + id + videoMediaExt;
+            vm.assetHref = {
+                assetThumb: vm.assetThumbPng,
+                assetVideo: vm.assetVideo
+            };
         };
     }
 
